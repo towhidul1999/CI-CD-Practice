b43059caa8e2bd0bed3d244ea008a593e94e15af
unknown <towhidulsislam@gmail.com>
2025-09-04 22:34:57 +0600

    feat:new docker iamge

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 import app from "./app.js";
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`✅ Server running on http://localhost:${port}`));
+app.listen(por, () => console.log(`✅ Server running on http://localhost:${port}`));
