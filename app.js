7ca5cf55c958b1370b7c36347c52c8267ccdc0fd
unknown <towhidulsislam@gmail.com>
2025-09-04 19:26:33 +0600

    feat: minimal express ci/cd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from Express + GitHub Actions 👋"));
+app.get("/", (req, res) => res.send("Hello from Express + GitHub Actions"));
 app.get("/health", (req, res) => res.json({ ok: true, service: "express-gha-demo" }));
 
 export default app;
