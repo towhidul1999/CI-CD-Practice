1c98d6fa2cd16ec1d7e9e75c06dd14afec3cc221
unknown <towhidulsislam@gmail.com>
2025-09-04 23:52:13 +0600

    New uddate docker

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from Express + GitHub Actions"));
+app.get("/", (req, res) => res.send("Hello from Express Docker + GitHub Actions"));
 app.get("/health", (req, res) => res.json({ ok: true, service: "express-gha-demo" }));
 
 export default app;
