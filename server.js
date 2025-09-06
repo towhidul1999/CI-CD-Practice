2c3637b17c28b91d8a0ce39e2bedc63d30ff0013
unknown <towhidulsislam@gmail.com>
2025-09-06 13:51:16 +0600

    Test auto-deploy

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import app from "./app.js";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Serverrrrr running on http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
